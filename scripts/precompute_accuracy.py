50a6f8ced1a3eb119299dfe76fdb834059d6ced1
Piorosen <chacha@udon.party>
2024-10-21 07:40:47 -0400

    test

--- scripts/precompute_accuracy.py
+++ scripts/precompute_accuracy.py
@@ -7,48 +7,48 @@ import argparse
 from utils import test, get_data_dataset
 import time
 import pickle
+import gc
 
-model_dict = {
-    "alexnet": tv.models.alexnet(True, True),
-    "densenet121": tv.models.densenet121(True, True),
-    "densenet161": tv.models.densenet161(True, True),
-    "densenet201": tv.models.densenet201(True, True),
-    "googlenet": tv.models.googlenet(True, True),
-    "resnet18": tv.models.resnet18(True, True),
-    "resnet34": tv.models.resnet34(True, True),
-    "resnet50": tv.models.resnet50(True, True),
-    "resnet101": tv.models.resnet101(True, True),
-    "resnet152": tv.models.resnet152(True, True),
-    "inception_v3": tv.models.inception_v3(True, True),
-    "vgg11": tv.models.vgg11(True, True),
-    "vgg11_bn": tv.models.vgg11_bn(True, True),
-    "vgg13": tv.models.vgg13(True, True),
-    "vgg13_bn": tv.models.vgg13_bn(True, True),
-    "vgg16": tv.models.vgg16(True, True),
-    "vgg16_bn": tv.models.vgg16_bn(True, True),
-    "vgg19": tv.models.vgg19(True, True),
-    "vgg19_bn": tv.models.vgg19_bn(True, True),
-    "mobilenet_v2": tv.models.mobilenet_v2(True, True),
-    "resnext50_32x4d": tv.models.resnext50_32x4d(True, True),
-    "resnext101_32x8d": tv.models.resnext101_32x8d(True, True),
-    "shufflenet_v2_x0_5": tv.models.shufflenet_v2_x0_5(True, True),
-    "shufflenet_v2_x1_0": tv.models.shufflenet_v2_x1_0(True, True),
-    "squeezenet1_0": tv.models.squeezenet1_0(True, True),
-    "squeezenet1_1": tv.models.squeezenet1_1(True, True)
-}
+torch.manual_seed(42)
 
-def execute(name, device, args):
-    if name in list(model_dict.keys()):
-        return False
-    
-    model = model_dict[name].to(device)
+def lazy_load():
+    return  {
+        "alexnet": tv.models.alexnet,
+        "densenet121": tv.models.densenet121,
+        "densenet161": tv.models.densenet161,
+        "densenet201": tv.models.densenet201,
+        "googlenet": tv.models.googlenet,
+        "resnet18": tv.models.resnet18,
+        "resnet34": tv.models.resnet34,
+        "resnet50": tv.models.resnet50,
+        "resnet101": tv.models.resnet101,
+        "resnet152": tv.models.resnet152,
+        "inception_v3": tv.models.inception_v3,
+        "vgg11": tv.models.vgg11,
+        "vgg11_bn": tv.models.vgg11_bn,
+        "vgg13": tv.models.vgg13,
+        "vgg13_bn": tv.models.vgg13_bn,
+        "vgg16": tv.models.vgg16,
+        "vgg16_bn": tv.models.vgg16_bn,
+        "vgg19": tv.models.vgg19,
+        "vgg19_bn": tv.models.vgg19_bn,
+        "mobilenet_v2": tv.models.mobilenet_v2,
+        "resnext50_32x4d": tv.models.resnext50_32x4d,
+        "resnext101_32x8d": tv.models.resnext101_32x8d,
+        "shufflenet_v2_x0_5": tv.models.shufflenet_v2_x0_5,
+        "shufflenet_v2_x1_0": tv.models.shufflenet_v2_x1_0,
+        "squeezenet1_0": tv.models.squeezenet1_0,
+        "squeezenet1_1": tv.models.squeezenet1_1
+    }
+
+def execute(model, name, device, args):
     _, val_loader, criterion = get_data_dataset('imagenet', args.dataset_directory, args.batch_size, args.test_batch_size)
     
     start = time.time()
     metric_logger = test(model, device, criterion, val_loader)
     end = time.time() - start
     with open(f'{name}_{end}', 'wb') as f:
-        pickle.dump([metric_logger, end, name, 'imagenet', device])
+        pickle.dump([metric_logger, end, name, 'imagenet', device], f)
     return True
 
 def main():
@@ -56,34 +56,36 @@ def main():
     parser.add_argument('--torch_home', type=str, help="Path to set as TORCH_HOME", default='/work/experiments')
     parser.add_argument('--output', type=str, help="Path to set as Exporting Path", default='/work/scripts/experiments')
     parser.add_argument('--dataset_directory', type=str, help="Path to set as dataset_directory", default='/work/dataset')
-    parser.add_argument('--batch_size', help="Path to set as dataset_directory", default=512, type=int)
-    parser.add_argument('--test_batch_size', help="Path to set as dataset_directory", default=512, type=int)
+    parser.add_argument('--batch_size', help="Path to set as dataset_directory", default=128, type=int)
+    parser.add_argument('--test_batch_size', help="Path to set as dataset_directory", default=128, type=int)
     parser.add_argument('--model', help="Path to set as dataset_directory", default='all', type=str)
     
     # Parse the arguments
     args, unknown = parser.parse_known_args()
-
+    print(args)
+    
     os.makedirs(args.output, exist_ok=True)
     # If --torch_home is provided, set it as the TORCH_HOME environment variable
     os.environ['TORCH_HOME'] = args.torch_home
-
-    torch.manual_seed(42)
+    print(os.getenv("TORCH_HOME"))
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_dict = lazy_load()
     
     if args.model == 'all':
         for name in list(model_dict.keys()):
-            execute(name, device, args)
+            model = model_dict[name](True,True).to(device)
+            execute(model, name, device, args)
+            del model
+        gc.collect()
     else:
-        execute(args.model, device, args)
-    
+        if name in list(model_dict.keys()):
+            return False
+        model = model_dict[name](True,True).to(device)
+        execute(model, name, device, args)
+        del model
+        
 if __name__ == "__main__":
     main()
-
-# %%
-
-# %%
-
-# %%
 
 # %%
--- scripts/utils.py
+++ scripts/utils.py
@@ -10,7 +10,8 @@ import torch.distributed as dist
 
 #%%
 def get_data_dataset(dataset, data_dir, batch_size, test_batch_size):
-    kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {
+    # kwargs = {'num_workers': 32+8, 'pin_memory': True} if torch.cuda.is_available() else {
+    kwargs = {'num_workers': 16, 'pin_memory': True, 'prefetch_factor': 4} if torch.cuda.is_available() else {
     }
     
     if dataset == 'cifar10':
@@ -130,6 +131,7 @@ def test(model, device, criterion, val_loader):
             metric_logger.update(loss=loss.item())
             metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
             metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+    
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
 
